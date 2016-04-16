d2c80d4e8d2f4e0ad1cb3fd146e61d663e4b6400
Smurfffy <alanmurphy1995@gmail.com>
2016-04-16 13:48:52 +0100

    Added Logo, facebook and twitter buttons and search

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var TG4NuachtApp = angular.module('starter', ['ionic'])
+var TG4NuachtApp = angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -42,4 +42,4 @@ TG4NuachtApp.controller("FeedController", function($http, $scope) {
         window.open(v, "_system", "location=yes");
     }
  
-});           
+});      
